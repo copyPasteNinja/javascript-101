ab66d19ecace57131412f8d921a0026d52351a8b
akumoproject <support@akumoproject.com>
2023-06-22 15:31:15 -0500

    basics

---
+++ basics.js
@@ -0,0 +1,29 @@
+"This_is_cool".length;
+
+'It\'s six o\'clock.';
+
+
+ // booleans 
+var kitchenLights = false;
+kitchenLights = true;
+kitchenLights;
+
+
+// variables
+
+var x = 100; 
+var y = x + 55;
+y; 
+
+var weather = "rainy";
+weather = "sunny";
+weather;
+
+// functions 
+
+function simpleMath(x, y) {
+    return x + y;
+}
+
+simpleMath(5, 5);
+
